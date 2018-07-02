1c226886d94fc287571206c9482da78ac1711a48
Julie Manhes <julie.manhes@hetic.net>
2018-07-02 23:25:42 +0200

    add responsive

---
+++ app/js/script.js
@@ -0,0 +1,32 @@
+// BURGER NAV //
+
+var navbar = document.querySelector('.header--nav');
+
+document.addEventListener('scroll', function (event) {
+	if (document.body.scrollTop  > 80 || event.pageY > 80) {
+      navbar.classList.add('sticky');
+  }
+  else if (document.body.scrollTop  < 80 || event.pageY < 80){
+    navbar.classList.remove('sticky');
+  }
+ });
+console.log("caak");
+var burger = document.querySelectorAll(".header--nav-burger div");
+var burger1 = document.querySelector(".burger-1");
+var burger2 = document.querySelector(".burger-2");
+var burger3 = document.querySelector(".burger-3");
+
+if (burger) {
+  for (let i = 0; i < burger.length; i++) {
+    burger[i].addEventListener("click", function() {
+      navbar.classList.toggle("hidden-nav");
+      burger1.classList.toggle("burger-transform-1");
+      burger2.classList.toggle("burger-transform-3");
+      burger3.classList.toggle("burger-transform-2");
+    });
+  }
+}
+
+
+
+
